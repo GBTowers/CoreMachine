aeba005fdfc27f95c77450fe91dd4eeef9caccb0
GBTowers <147354545+GBTowers@users.noreply.github.com>
2025-03-11 20:58:52 -0600

    Add tests to solution

---
+++ CoreMachine.Result.Test/ResultTest.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreMachine.Result.Test;
+
+public class ResultTest
+{
+    [Fact]
+    public void ResultThrowsIfNull()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var r = Result.Ok<string, string>(null!);
+        });
+
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var r = Result.Error<string, string>(null!);
+        });
+    }
+    
+    
+}
