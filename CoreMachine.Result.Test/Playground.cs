6c7a0a430848594ced11b63139f740abb21c2daf
Jorge-Torres <jorgearmandoz.x@gmail.com>
2025-03-12 21:53:10 -0600

    switch statement as match

--- CoreMachine.Result.Test/Playground.cs
+++ CoreMachine.Result.Test/Playground.cs
@@ -5,16 +5,30 @@ public class Playground
     [Fact]
     public void Play()
     {
-        var result = Result.Ok<int, string>(5);
-        if (result is IOk<int> ok)
+        Result<int, string> result = Result.Ok<int, string>(5);
+        if (result is IOk<int> success)
         {
-            Assert.Equal(10, ok.Value + 5);
+            Assert.Equal(10, success.Value + 5);
         }
 
         var errorResult = Result.Error<int, string>("This is an error");
-        if (errorResult is IErr<string> err)
+        if (errorResult is IErr<string> error)
         {
-            Assert.Equal("This is an error", err.Error);
+            Assert.Equal("This is an error", error.Error);
         }
+        
+        string match = result switch
+        {
+            IOk<int> ok => $"Value is {ok.Value}",
+            IErr<string> err => err.Error,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        string match2 = result.Match(
+            ok => $"Value is {ok}",
+            err => err);
+        
+        Assert.Equal("Value is 5", match);
+        Assert.Equal("Value is 5", match2);
     }
 }
