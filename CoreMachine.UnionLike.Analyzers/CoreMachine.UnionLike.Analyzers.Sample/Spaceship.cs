fc4ee9596c432ca2e80d8e9e771ca79c31769934
Jorge-Torres <jorgearmandoz.x@gmail.com>
2025-06-09 18:44:47 -0600

    unnecessary project

--- CoreMachine.UnionLike.Analyzers/CoreMachine.UnionLike.Analyzers.Sample/Spaceship.cs
+++
@@ -1,8 +0,0 @@
-using static System.ArgumentOutOfRangeException;
-
-namespace CoreMachine.UnionLike.Analyzers.Sample;
-
-public class Spaceship
-{
-	public static void SetSpeed(long speed) => ThrowIfGreaterThan(value: speed, other: 299_792_458);
-}
